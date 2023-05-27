e73930b32dcac26c9ce9e98dbec2f97930088fe9
Fernando Sutter <fernando.sutter1@gmail.com>
2023-05-27 19:08:12 -0300

    Correção vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './vue-pomodoro/',
+  base: '/vue-pomodoro/',
   plugins: [vue()],
   resolve: {
     alias: {
